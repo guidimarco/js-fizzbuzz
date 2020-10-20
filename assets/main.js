8baacaebc997c68462273b6b669b86039caf23f1
Marco Guidi <mark.88g@gmail.com>
2020-10-20 14:56:58 +0200

    fizz-buzz 2.1
    tolto valore di default e messo nel ramo else

--- assets/main.js
+++ assets/main.js
@@ -19,6 +19,37 @@
 // </1-fold END
 
 // <1-fold START --- FIZZ-BUZZ ALGORITHM VERSION 2.0 --- with stamp variable
+// // VAR ASSIGNMENT
+// // for stamp the 3-5 multiple
+// var stamp;
+//
+// // ALGORITHM FIZZ-BUZZ
+// // FROM 1 TO 100
+// for (var i = 1; i <= 100; i++) {
+//     // NOT MULTIPLE DEFAULT VALUE
+//     stamp = i;
+//
+//     // 3-MULTIPLE OR 5-MULTIPLE
+//     if (!(i % 3) || !(i % 5)) {
+//         stamp = "";
+//
+//         // 3-MULTIPLE
+//         if (!(i % 3)) {
+//             stamp = stamp + "Fizz"
+//         }
+//
+//         // 5-MULTIPLE
+//         if (!(i % 5)) {
+//             stamp = stamp + "Buzz"
+//         }
+//     }
+//
+//
+//     console.log(stamp);
+// }
+// </1-fold END
+
+// <1-fold START --- FIZZ-BUZZ ALGORITHM VERSION 2.1
 // VAR ASSIGNMENT
 // for stamp the 3-5 multiple
 var stamp;
@@ -26,8 +57,6 @@ var stamp;
 // ALGORITHM FIZZ-BUZZ
 // FROM 1 TO 100
 for (var i = 1; i <= 100; i++) {
-    // NOT MULTIPLE DEFAULT VALUE
-    stamp = i;
 
     // 3-MULTIPLE OR 5-MULTIPLE
     if (!(i % 3) || !(i % 5)) {
@@ -42,6 +71,9 @@ for (var i = 1; i <= 100; i++) {
         if (!(i % 5)) {
             stamp = stamp + "Buzz"
         }
+    } else {
+        // NOT MULTIPLE DEFAULT VALUE
+        stamp = i;
     }
 
 
