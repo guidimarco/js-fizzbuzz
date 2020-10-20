227183aa0186593293b079e77e14e3b0dfcd0c14
Marco Guidi <mark.88g@gmail.com>
2020-10-20 15:13:27 +0200

    correzione stampa nel log

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,23 @@
 
 // <1-fold START --- FIZZ-BUZZ ALGORITHM VERSION 1.0
-// // FROM 1 TO 100
-// for (var i = 1; i <= 100; i++) {
-//     if (i % 3 && i % 5) {
-//         // NOT MULTIPLE
-//         console.log(i);
-//     } else {
-//         // MULTIPLE
-//         if (!(i % 3) && !(i % 5)) {
-//             console.log("FizzBuzz");
-//         } else if (!(i % 3)) {
-//             console.log("Fizz");
-//         } else {
-//             console.log("Buzz");
-//         }
-//     }
-// }
+console.log("Fizz-Buzz Version 1.1:");
+
+// FROM 1 TO 100
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 && i % 5) {
+        // NOT MULTIPLE
+        console.log(i);
+    } else {
+        // MULTIPLE
+        if (!(i % 3) && !(i % 5)) {
+            console.log("FizzBuzz");
+        } else if (!(i % 3)) {
+            console.log("Fizz");
+        } else {
+            console.log("Buzz");
+        }
+    }
+}
 // </1-fold END --- FIZZ-BUZZ ALGORITHM VERSION 1.0
 
 // <1-fold START --- FIZZ-BUZZ ALGORITHM VERSION 2.0 --- with stamp variable
@@ -50,6 +52,7 @@
 // </1-fold END --- FIZZ-BUZZ ALGORITHM VERSION 2.0
 
 // <1-fold START --- FIZZ-BUZZ ALGORITHM VERSION 2.1
+console.log("Fizz-Buzz Version 2.1:");
 // VAR ASSIGNMENT
 // for stamp the 3-5 multiple
 var stamp;
